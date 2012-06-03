024fee55daa6b8e03bcd292e847a82ba3ba44fc7
Jesse House <jesse.house@gmail.com>
2012-06-03 12:10:21 -0700

    initial commit

---
+++ src/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schemadia::Application.config.secret_token = '455e3936825e72ce32fa7069bc06ee4c5f33e29a131faac29f7a51fca608748dedcf5f09bffae7d093b162b88214a9622c1b9d0317591cf2ff3697907095a308'
